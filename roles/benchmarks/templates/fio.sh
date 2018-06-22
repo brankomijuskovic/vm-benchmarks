c59dfcf93854bdb453f152c0239dd839e8c1fba0
Branko <brankomijuskovic@gmail.com>
2018-06-22 19:08:35 +0200

    Fine-tunning FIO script.

--- roles/benchmarks/templates/fio.sh
+++ roles/benchmarks/templates/fio.sh
@@ -5,7 +5,7 @@ TIME=$(date +%d-%m-%Y-%H-%M-%S)
 
 RESULTRAND(){
         LATMSorUS=$(grep -i lat results-$TEST-$TIME.txt | grep -vE "(slat|clat|platform)" | grep avg | awk '{print $2}')
-    	LAT=$(grep -i lat results-$TEST-$TIME.txt | grep -vE "(slat|clat|platform)" | head -n 1 | awk -F ',' '{print $3}')
+        LAT=$(grep -i lat results-$TEST-$TIME.txt | grep -vE "(slat|clat|platform)" | head -n 1 | awk -F ',' '{print $3}')
         BW=$(grep -i "bw=" results-$TEST-$TIME.txt | tr " " "\n" | grep BW)
         IOPS=$(grep -i "iops=" results-$TEST-$TIME.txt | tr " " "\n" | grep -i iops)
         echo "$IOPS $BW Lat$LATMSorUS $LAT"
@@ -25,16 +25,16 @@ RESULTSEQ(){
 # ioengine=sync, iodepth always is 1 (no matter what we set it to)
 # ioengine=libaio, iodepth is configurable (outstanding IO in the queue, per thread/job)
 # numjobs = double the CPU ? 8 for inside single VM...
-RANDREAD1(){ fio --runtime={{ fio_runtime }} --time_based --clocksource=clock_gettime --name=randread --iodepth=128 --numjobs=8 --rw=randread --bs=4k --size={{ fio_size }} --ioengine=libaio --filename=fio.tmp --sync=0 --direct=1 --group_reporting > results-$TEST-$TIME.txt; }
-RANDWRITE1(){ fio --runtime={{ fio_runtime }} --time_based --clocksource=clock_gettime --name=randwrite --iodepth=128 --numjobs=8 --rw=randwrite --bs=4k --size={{ fio_size }} --ioengine=libaio --filename=fio.tmp --sync=0 --direct=1 --group_reporting > results-$TEST-$TIME.txt; }
+RANDREAD1(){ fio --runtime={{ fio_runtime }} --time_based --clocksource=clock_gettime --name=randread --iodepth=4 --numjobs={{ ansible_processor_vcpus }} --rw=randread --bs=4k --size={{ fio_size }} --ioengine=libaio --filename=fio.tmp --sync=0 --direct=1 --group_reporting > results-$TEST-$TIME.txt; }
+RANDWRITE1(){ fio --runtime={{ fio_runtime }} --time_based --clocksource=clock_gettime --name=randwrite --iodepth=4 --numjobs={{ ansible_processor_vcpus }} --rw=randwrite --bs=4k --size={{ fio_size }} --ioengine=libaio --filename=fio.tmp --sync=0 --direct=1 --group_reporting > results-$TEST-$TIME.txt; }
 #libaio, sync=1
-RANDREAD2(){ fio --runtime={{ fio_runtime }} --time_based --clocksource=clock_gettime --name=randread --iodepth=128 --numjobs=8 --rw=randread --bs=8k --size={{ fio_size }} --ioengine=libaio --filename=fio.tmp --sync=1 --direct=1 --group_reporting > results-$TEST-$TIME.txt; }
-RANDWRITE2(){ fio --runtime={{ fio_runtime }} --time_based --clocksource=clock_gettime --name=randwrite --iodepth=128 --numjobs=8 --rw=randwrite --bs=8k --size={{ fio_size }} --ioengine=libaio --filename=fio.tmp --sync=1 --direct=1 --group_reporting > results-$TEST-$TIME.txt; }
+RANDREAD2(){ fio --runtime={{ fio_runtime }} --time_based --clocksource=clock_gettime --name=randread --iodepth=4 --numjobs={{ ansible_processor_vcpus }} --rw=randread --bs=8k --size={{ fio_size }} --ioengine=libaio --filename=fio.tmp --sync=1 --direct=1 --group_reporting > results-$TEST-$TIME.txt; }
+RANDWRITE2(){ fio --runtime={{ fio_runtime }} --time_based --clocksource=clock_gettime --name=randwrite --iodepth=4 --numjobs={{ ansible_processor_vcpus }} --rw=randwrite --bs=8k --size={{ fio_size }} --ioengine=libaio --filename=fio.tmp --sync=1 --direct=1 --group_reporting > results-$TEST-$TIME.txt; }
 
 
 # Sequential read/write tests, no variants needed
-SEQREAD(){ fio --runtime={{ fio_runtime }} --time_based --clocksource=clock_gettime --name=read --iodepth=128 --numjobs=8 --rw=read --bs=64k --size={{ fio_size }} --ioengine=libaio --filename=fio.tmp --sync=0 --direct=1 --group_reporting > results-$TEST-$TIME.txt; }
-SEQWRITE(){ fio --runtime={{ fio_runtime }} --time_based --clocksource=clock_gettime --name=write --iodepth=128 --numjobs=8 --rw=write --bs=64k --size={{ fio_size }} --ioengine=libaio --filename=fio.tmp --sync=0 --direct=1 --group_reporting > results-$TEST-$TIME.txt; }
+SEQREAD(){ fio --runtime={{ fio_runtime }} --time_based --clocksource=clock_gettime --name=read --iodepth=4 --numjobs={{ ansible_processor_vcpus }} --rw=read --bs=64k --size={{ fio_size }} --ioengine=libaio --filename=fio.tmp --sync=0 --direct=1 --group_reporting > results-$TEST-$TIME.txt; }
+SEQWRITE(){ fio --runtime={{ fio_runtime }} --time_based --clocksource=clock_gettime --name=write --iodepth=4 --numjobs={{ ansible_processor_vcpus }} --rw=write --bs=64k --size={{ fio_size }} --ioengine=libaio --filename=fio.tmp --sync=0 --direct=1 --group_reporting > results-$TEST-$TIME.txt; }
 
 
 #echo "Starting FIO `date`"
